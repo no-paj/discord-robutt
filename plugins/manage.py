1b37eee9d1f5e25824b01e081bbd322cd8ee4268
no-paj <nopaj@openmailbox.org>
2015-11-10 17:36:35 +0100

    #

--- plugins/manage.py
+++ plugins/manage.py
@@ -15,17 +15,27 @@ class Manage(Plugin):
 	def on_message(self, message):
 		self.join(message)
 		self.leave(message)
+		self.server_list(message)
 
 	def __init__(self, core):
 		Plugin.__init__(self, core=core)
 
 		self.join = Protector(config['ADMINS'])(self.join)
+		self.leave = Protector(config['ADMINS'])(self.leave)
 
-	@Command('^join https://discord\.gg/([A-Za-z0-9]*)$')
+	@Command('^(join|j) https://discord\.gg/([A-Za-z0-9]*)$')
 	def join(self, message):
-		if self.core.accept_invite('https://discord.gg/{}'.join(message.options[0])):
+		if self.core.accept_invite('https://discord.gg/{}'.format(message.options[1])):
 			self.core.send_message(message.channel, 'Server joined !')
 
-	@Command('leave')
+	@Command('^leave$')
 	def leave(self, message):
 		self.core.leave_server(message.channel.server)
+
+	@Command('^servers$')
+	def server_list(self, message):
+		response = ''
+		for server in self.core.servers:
+			response += '{} | {} connected\n'.format(server.name, len(server.members))
+		response = '```\n {}```'.format(response)
+		self.core.send_message(message.channel,response)
